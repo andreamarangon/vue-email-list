beed5a8c590a5cb60ec657b84fa443108058e5b4
Andrea Marangon <marangonandrea84@gmail.com>
2021-04-09 10:03:54 +0200

    aggiunta bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,29 @@
+Vue.config.devtools = true;
+
 var app = new Vue({
   el: '#root',
   data: {
     mails: []
   },
-  // mounted(){
-  //   const self = this;
-  //   for (var i = 0; i < 10; i++) {
-  //     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-  //      .then(function (result) {
-  //        self.mails.push(result.data.response);
-  //     });
-  //
-  //   }
-  // },
-  methods: {
-    generaMail: function () {
-      const self = this;
-      for (var i = 0; i < 10; i++) {
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-         .then(function (result) {
-           self.mails.push(result.data.response);
-        });
-      }
+  mounted(){
+    for (var i = 0; i < 10; i++) {
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+       .then((result) => {
+         this.mails.push(result.data.response);
+      });
+
     }
-  }
+  },
+  // methods: {
+  //   generaMail: function () {
+  //     for (var i = 0; i < 10; i++) {
+  //       axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+  //           .then((result) => {
+  //             this.mails.push(result.data.response);
+  //       });
+  //     }
+  //   }
+  // }
 
 
 });
